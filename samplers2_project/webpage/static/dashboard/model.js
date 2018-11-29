a90b5011ea87fab9a9acb2f4197eda8c50715fc6
Alex Rojas <alex.rojas@despegar.com>
2018-11-29 12:07:36 -0300

    Fix para mostrar icono de option y requirment.txt

--- samplers2_project/webpage/static/dashboard/model.js
+++ samplers2_project/webpage/static/dashboard/model.js
@@ -1,3 +1,6 @@
+
+var image123 = "{% static "dashboard/option.png" %}"
+
 class Workflow {
 	constructor(steps,nodestypes) {
 		this.steps = steps;
@@ -352,13 +355,16 @@ class Multiple extends Step {
 			}
 		}
 	}
+
+
 	getNodesToRender(father, level) {
+		console.log(image123)
 		nodes.push({id: this.id , shape: 'circularImage', image:this.getNodeType().icon, 'level' : level  , label: String(this.id)});
 		if (this.options){
 			for (var i = 0; i < this.options.length; i++) {
 				if(this.options[i]){
 					var optionId = this.options[i].id;
-					nodes.push({id: optionId,  'level' : level+1  , label: String("option: " +optionId)});
+					nodes.push({id: optionId, shape: 'circularImage', 'image':"/static/dashboard/option.png", 'level' : level+1  , label: String("option: " +optionId)});
 				}
 
 
